670ec7518a5036312630c05d87adfee0566f2039
David Sutherland <sutherdg@gmail.com>
2022-03-17 00:06:51 +0000

    Added functions

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
-
+    public static void announceDeveloperTeaTime() {
+        System.out.println("Waiting for developer team time...");
+        System.out.println("Type in a random word and press Enter");
+        Scanner input = new Scanner(System.in);
+        input.next();
+        System.out.println("It's developer team time!");
+    }
     public static void main(String[] args) {
+        System.out.println("Welcome to your new job");
 
+        announceDeveloperTeaTime();
+
+        System.out.println("Write code");
+        System.out.println("Review code");
+
+        announceDeveloperTeaTime();
+
+        System.out.println("Get promoted!");
     }
 
 }
